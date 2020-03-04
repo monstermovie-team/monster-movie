aac96ddd8a0d582ac8710b9edc7ff1f71b80263e
BoomShaGa1aGa <2524799295@qq.com>
2020-03-04 19:37:05 +0800

    实现登录功能

--- movie-pcweb/src/main/java/edu/nf/movie/customer/pcweb/config/SecurityConfig.java
+++ movie-pcweb/src/main/java/edu/nf/movie/customer/pcweb/config/SecurityConfig.java
@@ -111,8 +111,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**",
-                "/**/*.html", "/**/*.jpg", "/**/*.png", "/**/*.ico");
+        web.ignoring().antMatchers("/*.html","/**/*.css","/**/*.js");
     }
 
     /**
