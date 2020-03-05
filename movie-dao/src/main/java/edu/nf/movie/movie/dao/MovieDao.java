f4d4792924b0683b619d6eae94c3ba13ebe9beb9
coder_fu <1161789067@qq.com>
2020-03-05 21:40:50 +0800

    付金鑫

--- movie-dao/src/main/java/edu/nf/movie/movie/dao/MovieDao.java
+++ movie-dao/src/main/java/edu/nf/movie/movie/dao/MovieDao.java
@@ -1,6 +1,9 @@
 package edu.nf.movie.movie.dao;
 
 import edu.nf.movie.movie.entity.MovieInfo;
+import edu.nf.movie.movie.entity.MovieRegion;
+import edu.nf.movie.movie.entity.MovieType;
+import edu.nf.movie.movie.entity.MovieYear;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -14,4 +17,13 @@ import java.util.List;
 @Repository
 public interface MovieDao {
     List<MovieInfo> movieInfoList(@Param("pageNum") Integer pageNum, @Param("pageSize") Integer pageSize);
+
+    List<MovieInfo> movieInfoListByState(@Param("pageNum") Integer pageNum, @Param("pageSize") Integer pageSize ,Integer State);
+
+    List<MovieType> movieTypeList();
+
+    List<MovieYear> movieYearList();
+
+    List<MovieRegion> movieRegionList();
+
 }
