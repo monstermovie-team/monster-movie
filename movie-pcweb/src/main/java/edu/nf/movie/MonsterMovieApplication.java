01d667b7fcd88e41ef8fdc21283fc57c2b4c3eac
qianf <q603263371lovehqf@foxmail.com>
2020-03-05 14:22:34 +0800

    黄芊富

--- movie-pcweb/src/main/java/edu/nf/movie/MonsterMovieApplication.java
+++ movie-pcweb/src/main/java/edu/nf/movie/MonsterMovieApplication.java
@@ -1,5 +1,6 @@
 package edu.nf.movie;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020-03-02
  */
 @SpringBootApplication
-public class MonsterMovieApplication {
+@MapperScan("edu.nf.movie.customer.dao")
+public class  MonsterMovieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonsterMovieApplication.class);
