09b3f5de28ed5d08b7e867f8470a5865c325a53e
八二DB <1833429338@qq.com>
2020-03-08 23:00:21 +0800

    11

---
+++ movie-pcweb/src/main/resources/static/js/cinma.js
@@ -0,0 +1,8 @@
+
+// $(".brand-content").on('click','.zone-link',function(){//点击事件
+// 	 $(this).siblings().css("border-radius":"14px","padding":"3px9px",
+// 	"display": "inline-block","margin-left": "12px","background": "#f34d41","color": "#fff");
+// 	alert("aa");
+//  })
+
+
